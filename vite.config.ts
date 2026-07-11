e2b5344e48c7344f5ac95a8c0da46f8196b0b20c
yoshikazzz <yoshikazzzz@gmail.com>
2026-07-12 03:31:19 +0900

    Configure custom domain jul-2026.josh-nish.com

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/jul-2026/',
+  base: '/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
